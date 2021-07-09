4722052aa97580e4f4a116247b6c2143a5bb36fd
kid-on-github <mike@lemonshell.com>
2021-07-09 16:28:05 -0700

    chunking post requests

--- backend/functions/function.js
+++ backend/functions/function.js
@@ -7,86 +7,112 @@ const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 
 
-// save to dynamo
-async function saveData(A,B,C){
-
-    const params = {
-        RequestItems: {
-            Hello: [
-                {
-                    PutRequest: {
-                        Item: {
-                            'pk' : {S: A[0]},
-                            'sk' : {S: A[1]}
-                        }
-                    }
-                },
-                {
-                    PutRequest: {
-                        Item: {
-                            'pk' : {S: B[0]},
-                            'sk' : {S: B[1]}
-                        }
-                    }
-                },
-                {
-                    PutRequest: {
-                        Item: {
-                            'pk' : {S: C[0]},
-                            'sk' : {S: C[1]}
-                        }
-                    }
-                }
-            ]
-            
+const record = (X) => {
+    return {
+        PutRequest: {
+            Item: {
+                'pk' : {S: X[0]},
+                'sk' : {S: X[1]}
+            }
         }
     }
+}
+
+
+
+function buildRecordSet(path, url, email){
+    // get important data points
     
+    const [user, domain] = email.split('@')
+    
+    const A = [`DOMAIN#${domain}`,user]
+    const B = [`URL#${url}`, email]
+    const C = [email, `PATH#${path}`]
+    
+    return [record(A), record(B), record(C)]
+}
+
+
+
+
+
+
+
+// save to dynamo
+async function saveData(records){
+    console.log('records',records)
     let response
-    try{response = await dynamodb.batchWriteItem(params).promise()}
-    catch(err){response=err}
+    try{
+        response = await dynamodb.batchWriteItem(
+            {
+                RequestItems: { Hello: records }
+            }
+        ).promise()
+    }
     
+    catch(err){response=err}
     return response
 }
 
 
 
-// main handler
-exports.handler = async function (event) {
-    
-    const {path, emails} = JSON.parse(event.body)
-    
-    console.log('SAVING DATA:')
-    
-    
-    
-    
 
-    const x = path.split('/')
-    const email = emails[0]
-    const url = 'https://lemonshell.com'
+
+async function buildRecords(path, emails){
+    console.log('SAVING EMAILS:', emails)
     
-    const [user, domain] = email.split('@')
     
-    const response = await saveData(
-        [`DOMAIN#${domain}`,user],
-        [`URL#${url}`, email],
-        [email, `PATH#${path}`]
+    
+    const url = path.split('/').slice(0,3).join('/')
+    
+    let records = []
+    
+    emails.map(
+        email => {
+            const recordSet = buildRecordSet(path,url,email)
+            records = records.concat(recordSet)
+        }
     )
     
+    console.log(records)
+    
+    const response = await saveData(records)
     
     console.log('RESPONSE', response)
+    return response
+}
+
+
+
+
+
+
+// main handler
+exports.handler = async function (event) {
+    const {path, emails} = JSON.parse(event.body)
+    await buildRecords(path, emails)
     return sendRes(200, event.body)
-};
+}
   
   
+
+
+
+
+
 
 // format response
 const sendRes = (status, body) => {
     var response = {
       statusCode: status,
-      headers: {"Content-Type": "text/html"},
+      headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        "Content-Type": "text/html"
+      },
       body: body,
-    };
-    return response;
-};
+    }
+    
+    return response
+}
--- extension/findEmails.js
+++ extension/findEmails.js
@@ -4,10 +4,13 @@ function findEmails(){
     
     let domains = {}
     let emailCount = 0
-    
+    let emailList = []
+
+
     for (let i of emails){
         try {
             let email = i.href.toLowerCase().split(':')[1]
+            emailList.push(email)
             let [username, domain] = email.split('@')
             
             !(domain in domains) && (domains[domain] = [])
@@ -20,6 +23,46 @@ function findEmails(){
 
     console.log(`Domains: ${Object.keys(domains).length} \nTotal Emails: ${emailCount}`)
 
+
+
+    const chunkSize = 8
+    let x = Math.ceil(emailList.length/chunkSize)
+
+    for (let i=0; i<x; i++){
+        const start = i * chunkSize
+        const end = start + chunkSize
+        const chunk = emailList.slice(start,end)
+        console.log('chunk',chunk)
+
+        saveEmails(window.location.href, chunk)
+    }
+}
+
+
+
+
+function saveEmails(path, emails){
+    console.log('Saving emails...')
+    let myHeaders = new Headers()
+    myHeaders.append("Content-Type", "application/json")
+
+    let raw = JSON.stringify({
+        "path": path,
+        "emails": emails
+    })
+    
+    let requestOptions = {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    }
+
+    fetch("https://fehuatfkq6.execute-api.us-east-1.amazonaws.com/prod/hello", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error))
 }
 
 
