3980587104b124ca5d47d359a72f5a9bf26804ae
kid-on-github <mike@lemonshell.com>
2021-07-12 13:44:36 -0700

    updating site

--- site/src/App.js
+++ site/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [results, setResults] = useState([])
+
+
+  const getEmailLocations = (event) => { 
+    if (event.key === 'Enter') {
+      
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+
+      setResults({email})
+      
+      fetch(`https://5aijchuyn4.execute-api.us-east-1.amazonaws.com/prod/get-email?email=${email}`, requestOptions)
+        .then(response => response.json())
+        .then(result => setResults({email, results:result.results}))
+        .catch(error => console.log('error', error));
+
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='search'>
+        <h1>friendlybot.org</h1>
+        <p className='fs24 fw300'>Where can <b className='color-blue'>YOUR</b> email be found?</p>
+        <div>
+          <input autoFocus type='email' id='emailInput' placeholder='you@example.com' onKeyUp={getEmailLocations} onChange={e=>setEmail(e.target.value)}/>
+          
+        </div>
+        <Results results={results}/>
+      </div>
     </div>
   );
 }
+
+
+
+
+
+function Results(props){
+  const {results} = props
+  console.log('r',results)
+
+  const loaded = 'results' in results
+
+  const renderResults = loaded
+    ? results.results.map(url => <p>{url}</p>)
+    : <p></p>
+
+
+  return (
+    <div className='results'>
+      {results.email && <p>Results for {results.email}</p>}
+      {renderResults}
+    </div>
+  )
+}
+
+
 
 export default App;
