507a6aec32148d1daad28da23ff23742146b0249
kid-on-github <mike@lemonshell.com>
2021-07-14 11:39:03 -0700

    added header

--- site/src/App.js
+++ site/src/App.js
@@ -6,28 +6,18 @@ function App() {
   const [email, setEmail] = useState('')
   const [results, setResults] = useState([])
 
-
-
-
   const getEmailLocations = () => { 
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
     setResults({email})
-    
-    fetch(`https://5aijchuyn4.execute-api.us-east-1.amazonaws.com/prod/get-email?email=${email}`, requestOptions)
+    fetch(`https://5aijchuyn4.execute-api.us-east-1.amazonaws.com/prod/get-email?email=${email}`)
       .then(response => response.json())
       .then(result => setResults({email, results:result.results}))
       .catch(error => console.log('error', error));
-
   }
 
 
   return (
     <div className="App">
-
+      <Header/>
       <div className='search'>
         <h1>friendlybot.org</h1>
         <p className='fs24 fw300'>Where can <b className='color-blue'>YOUR</b> email be found?</p>
@@ -44,6 +34,25 @@ function App() {
 
 
 
+function Header(props){
+  const [clicked, setClicked] = useState('home')
+
+  const anchor = document.URL.split('#')[1]
+
+  anchor && anchor !== clicked && setClicked(anchor)
+
+  return (
+    <header>
+      <a className={clicked === 'home' && 'selected'} onClick={()=>setClicked('home')} href='./'>home</a>
+      <a className={clicked === 'mission' && 'selected'} onClick={()=>setClicked('mission')} href='./#mission'>mission</a>
+      <a href='./'>docs</a>
+      <a className='ul-blue' href='./'>download</a>
+    </header>
+  )
+}
+
+
+
 
 function Results(props){
   const {results} = props
@@ -52,13 +61,13 @@ function Results(props){
   const loaded = 'results' in results
 
   const renderResults = loaded
-    ? results.results.map(url => <p>{url}</p>)
+    ? results.results.map(url => <p className='fs20' key={url}>{url}</p>)
     : <p></p>
 
 
   return (
     <div className='results'>
-      {results.email && <p>Results for {results.email}</p>}
+      {results.email && <p className='fw500 fs30'>Results for <span className='color-blue'>{results.email}</span></p>}
       {renderResults}
     </div>
   )
