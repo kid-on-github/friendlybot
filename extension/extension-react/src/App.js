ee27782188d2d69906c879cf874235aaecb0cd2a
kid-on-github <mike@lemonshell.com>
2021-07-15 15:23:23 -0700

    search is working

--- extension/extension-react/src/App.js
+++ extension/extension-react/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 
 
+// APP
 function App() {
 
   const [page, setPage] = useState('search');
@@ -14,32 +15,34 @@ function App() {
   );
 }
 
-function Header(props){
 
+
+
+
+// HEADER
+function Header(props){
   const {page, setPage} = props
 
   const buttons = ['search', 'settings'].map(
-    icon => <IconButton 
-              className={page === icon ? 'selected' : ''} 
-              icon={icon}
-              setPage={setPage}
-            />
+    icon => <IconButton className={page === icon ? 'selected' : ''} icon={icon} setPage={setPage} key={icon}/>
   )
 
-  return (
-    <header>
-      {buttons}
-    </header>
-  )
+  return <header> {buttons} </header>
 }
 
 
+
+
+// ICON
 function Icon(props){
-  const {icon} = props
-  return <span className="material-icons icon">{icon}</span>
+  const {icon, onClick} = props
+  return <span className="material-icons icon" onClick={()=>onClick && onClick()}>{icon}</span>
 }
 
 
+
+
+// ICON BUTTON
 function IconButton(props){
   const {icon, className, setPage} = props
   return  (
@@ -49,29 +52,57 @@ function IconButton(props){
   )
 }
 
+
+
+
+
+// SEARCH PAGE
 function Search(){
+  const [results, setResults] = useState()
+
+  console.log(results)
+  
+  const getEmailLocations = (email) => { 
+    setResults({email})
+    fetch(`https://5aijchuyn4.execute-api.us-east-1.amazonaws.com/prod/get-email?email=${email}`)
+      .then(response => response.json())
+      .then(result => setResults({email, results:result.results}))
+      .catch(error => console.log('error', error));
+  }
+
+  const links = results && results.results && results.results.map(
+    link => <p>{link}</p>
+  )
+
   return (
-    <div className='search'>
+    <div className={'search ' + (results ? 'results' : '')}>
       <h1>friendlybot.org</h1>
-      <InputBar type='email' icon='search'/>
+      <InputBar type='email' icon='search' submit={getEmailLocations}/>
+      {links}
     </div>
   )
 }
 
 
+
+
+
+
+
+// INPUT BAR
 function InputBar(props){
-
-  const {type, icon} = props
-
+  const {type, icon, submit} = props
+  const [val, setVal] = useState('')
   return (
     <div className='inputBar'>
-      <input type={type}/>
-      <Icon icon={icon}/>
+      <input type={type} onChange={e=>setVal(e.target.value)} onKeyUp={e => e.key === 'Enter' && submit(val)}/>
+      <Icon icon={icon} onClick={()=>submit(val)}/>
     </div>
   )
 }
 
 
+// SETTINGS PAGE
 function Settings(){
   return (
     <div>
