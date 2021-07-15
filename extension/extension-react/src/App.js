6c635803da04268f53876a596dc1fc0d39ce6bd7
kid-on-github <mike@lemonshell.com>
2021-07-15 12:56:25 -0700

    working on chrome extension

--- extension/extension-react/src/App.js
+++ extension/extension-react/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [page, setPage] = useState('search');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header page={page} setPage={setPage}/>
+      {page === 'search' ? <Search/> : <Settings/>}
     </div>
   );
+}
+
+function Header(props){
+
+  const {page, setPage} = props
+
+  const buttons = ['search', 'settings'].map(
+    icon => <IconButton 
+              className={page === icon ? 'selected' : ''} 
+              icon={icon}
+              setPage={setPage}
+            />
+  )
+
+  return (
+    <header>
+      {buttons}
+    </header>
+  )
+}
+
+
+function Icon(props){
+  const {icon} = props
+  return <span className="material-icons icon">{icon}</span>
+}
+
+
+function IconButton(props){
+  const {icon, className, setPage} = props
+  return  (
+    <button className={className} onClick={() => setPage(icon)}>
+      <Icon icon={icon}/>
+    </button>
+  )
+}
+
+function Search(){
+  return (
+    <div className='search'>
+      <h1>friendlybot.org</h1>
+      <InputBar type='email' icon='search'/>
+    </div>
+  )
+}
+
+
+function InputBar(props){
+
+  const {type, icon} = props
+
+  return (
+    <div className='inputBar'>
+      <input type={type}/>
+      <Icon icon={icon}/>
+    </div>
+  )
+}
+
+
+function Settings(){
+  return (
+    <div>
+      
+    </div>
+  )
 }
 
 export default App;
